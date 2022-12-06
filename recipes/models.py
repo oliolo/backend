d7a2d1d4b2a0a03289e371861b8b6c114fecd935
Cloud327 <roman.ermolaev@hotmail.com>
2022-12-06 14:17:07 +0100

    created recipe model

--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+from django.urls import reverse
+
+
+# Create your models here.
+class Recipe(models.Model):
+    portionSize = models.BigIntegerField()
+    decription = models.TextField()
+    creationDate = models.DateField()
+    name = models.CharField(max_length=150)
+    creationDate.auto_now_add = True
+    slug = models.SlugField(null=True)
+    
+    def __str__(self):
+        return self.name
+    
+    def get_absolute_url(self):
+        return reverse("recipe_detail", kwargs={"slug": self.slug})
