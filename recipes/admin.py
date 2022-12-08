77bb82bfd67c28eed5122fc09632b0c9c95c620c
oliolo <oliver_olofsson@hotmail.se>
2022-12-08 19:25:41 +0100

    added views and urls

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from .models import Recipe
+
 # Register your models here.
+   
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ("name", "description", "portionSize", "creationDate")
+
+admin.site.register(Recipe, RecipeAdmin)
--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import RecipeSerializer
+from .models import Recipe
 
 # Create your views here.
+
+class RecipeView(viewsets.ModelViewSet):
+    serializer_class = RecipeSerializer
+    queryset = Recipe.objects.all()
+    lookup_field = 'slug'
