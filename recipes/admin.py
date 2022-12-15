94dd90e82d1a6365a2586d08361a64698cfbe429
Cloud327 <roman.ermolaev@hotmail.com>
2022-12-15 22:31:29 +0100

    DONT FORGET TO ADD SAVED RECIPES TO USERADMIN AFTER IT IS FIXED

--- recipes/admin.py
+++ recipes/admin.py
@@ -5,7 +5,7 @@ from .models import *
 # Register your models here.
    
 class UserAdmin(admin.ModelAdmin):
-    list_display = ("email", "name", "password", "savedRecipes")
+    list_display = ("email", "name", "password")
 class RecipeAdmin(admin.ModelAdmin):
     list_display = ("name", "description", "portionSize", "creationDate")
 class CategoryAdmin(admin.ModelAdmin):
