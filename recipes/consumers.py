59fbd096c839387214131be3d327bfa7f1cf3d54
oliolo <oliver_olofsson@hotmail.se>
2023-01-03 22:49:31 +0100

    fixes

--- recipes/consumers.py
+++ recipes/consumers.py
@@ -6,7 +6,7 @@ from channels.generic.websocket import WebsocketConsumer
 class TextRoomConsumer(WebsocketConsumer):
     def connect(self):
 
-        self.room_name = ''
+        self.room_name = self.scope['url_route']['kwargs']['room_name']
         self.room_group_name = 'recipes_%s' % self.room_name
         # Join room group
         async_to_sync(self.channel_layer.group_add)(
--- recipes/routing.py
+++ recipes/routing.py
@@ -2,5 +2,5 @@ from django.urls import re_path
 from recipes.consumers import TextRoomConsumer
 
 websocket_urlpatterns = [
-    re_path(r'^ws/(?P<room_name>[^/]+)/$', TextRoomConsumer.as_asgi()),
+    re_path(r"ws/(?P<room_name>\w+)/$", TextRoomConsumer.as_asgi()),
 ]
